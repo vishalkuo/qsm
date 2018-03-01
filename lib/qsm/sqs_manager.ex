f56d94fc90e3328f79873ff0fa8d0ad0208d5ab0
vishalkuo <vishalkuo@hotmail.com>
2018-02-28 19:22:34 -0800

    Adds docs

--- lib/qsm/sqs_manager.ex
+++ lib/qsm/sqs_manager.ex
@@ -1,6 +1,15 @@
 defmodule Qsm.SqsManager do
   @aws Application.get_env(:qsm, :aws)
 
+  @doc ~S"""
+  Message handler function responsible for parsing sqs messages and enqueueing
+  the next state.
+
+  ## Example
+      iex> msg = %Qsm.QueueMessage{module_name: Qsm.MockEntryState, body: "foo"}
+      iex> Qsm.SqsManager.message_handler("test_queue", Poison.encode!(msg))
+      :ok
+  """
   @spec message_handler(Qsm.queue_name(), String.t()) :: :ok
   def message_handler(queue_name, message) do
     case apply_transition(message) do
@@ -22,6 +31,13 @@ defmodule Qsm.SqsManager do
     Poison.encode!(message)
   end
 
+  @doc ~S"""
+  Function to place states on SQS queue. Used for both entry and transition
+
+  ## Example
+      iex> Qsm.SqsManager.send_message("test_queue", Qsm.MockEntryState, "foo")
+      :ok
+  """
   @spec send_message(Qsm.queue_name(), Qsm.State, Qsm.state_data()) :: :ok
   def send_message(queue_name, state, data) do
     message = create_message(state, data)
