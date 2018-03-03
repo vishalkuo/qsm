56f6825c355cabd4a7b057063925150f3f1e74b9
vishalkuo <vishalkuo@hotmail.com>
2018-03-02 21:48:20 -0800

    Adds hex data

--- mix.exs
+++ mix.exs
@@ -7,7 +7,11 @@ defmodule Qsm.Mixfile do
       version: "0.1.0",
       elixir: "~> 1.6.1",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      name: "qsm",
+      source_url: "https://github.com/vishalkuo/qsm",
+      package: package()
     ]
   end
 
@@ -22,6 +26,20 @@ defmodule Qsm.Mixfile do
       {:e_poller, "~> 0.1.1"},
       {:poison, "~> 3.1"},
       {:ex_doc, "~> 0.16", only: :dev, runtime: false}
+    ]
+  end
+
+  defp description() do 
+    "A finite state machine built on top of SQS."
+  end
+
+  defp package do
+    [
+      name: "qsm",
+      files: ["lib", "mix.exs", "README*", "LICENSE*"],
+      maintainers: ["Vishal Kuo", "Justin Trudell"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/vishalkuo/qsm"}
     ]
   end
 end
